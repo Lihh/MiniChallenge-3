55e7e714e548e60aa911b52993d82116e9cf4c75
Lidia Chou <lidia.chou94@gmail.com>
2015-05-15 10:59:23 -0300

    Criando a estrutura para jogo do corpo humano

---
+++ MiniChallenge3/MiniChallenge3/GameHumanBodyViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameHumanBodyViewController.swift
+//  MiniChallenge3
+//
+//  Created by Lidia Chou on 5/15/15.
+//  Copyright (c) 2015 Lidia Chou. All rights reserved.
+//
+
+import UIKit
+
+class GameHumanBodyViewController: UIViewController {
+
+    @IBOutlet weak var ImageBody: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+}
