6d3850d73cef0e2b926a49e446789af186658744
CaueAlvesSilva <cauesilva@MacBook-Pro-de-Caue.local>
2015-05-14 22:05:09 -0300

    Jogo "Descubra a Palavra" - versão 1
    - Estrutura do jogo criada
    - Animação para silabas erradas
    - Blur effect para acerto

--- MiniChallenge3/MiniChallenge3/SyllablesViewController.swift
+++ MiniChallenge3/MiniChallenge3/SyllablesViewController.swift
@@ -10,26 +10,168 @@ import UIKit
 
 class SyllablesViewController: UIViewController {
 
-    override func viewDidLoad() {
+    // MARK: - Properties and Outlets
+    //================================================================================
+    
+    // Level
+    var level: Int = 0
+    
+    // Levels Array for images and syllables
+    var levels: [( level:Int,    correctWord:String, correctImage:String,
+                  image1:String, syllable1:String,   deleteSyllable1:String,
+                  image2:String, syllable2:String,   deleteSyllable2:String,
+                  image3:String, syllable3:String,   deleteSyllable3:String)]
+    
+    // TESTE
+              = [(1, "JANELA",     "janela.jpg",
+                     "jacare.png", "JA", "CARÉ",
+                     "neve.png",   "NE", "VE",
+                     "lapis.png",  "LA", "PIS")]
+    
+    // Outlets
+    @IBOutlet weak var txtSyllable1: UITextField!
+    @IBOutlet weak var txtSyllable2: UITextField!
+    @IBOutlet weak var txtSyllable3: UITextField!
+    @IBOutlet weak var btnConfirm:   UIButton!
+    //================================================================================
+    
+    
+    
+    // MARK: - VC Lyfe Cycle
+    //================================================================================
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(animated: Bool)
+    {
+        // Border
+        txtSyllable1.layer.borderWidth = 2
+        txtSyllable2.layer.borderWidth = 2
+        txtSyllable3.layer.borderWidth = 2
+        btnConfirm.layer.borderWidth = 2
+        
+        // Border Color
+        txtSyllable1.layer.borderColor = UIColor .grayColor().CGColor
+        txtSyllable2.layer.borderColor = UIColor .grayColor().CGColor
+        txtSyllable3.layer.borderColor = UIColor .grayColor().CGColor
+        btnConfirm.layer.borderColor = UIColor .whiteColor().CGColor
+        
+        // Corner Radius
+        txtSyllable1.layer.cornerRadius = 10
+        txtSyllable2.layer.cornerRadius = 10
+        txtSyllable3.layer.cornerRadius = 10
+        btnConfirm.layer.cornerRadius = 20
+        
+        // Button Color
+        btnConfirm.layer.backgroundColor = UIColor .greenColor().CGColor
     }
-    */
 
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    //================================================================================
+
+    
+    
+    // MARK: - Confirm Answers
+    //================================================================================
+    @IBAction func confirm(sender: AnyObject)
+    {
+        // Correct Answer
+        //-------------------------------------------------
+        if (txtSyllable1.text == levels[level].syllable1 &&
+            txtSyllable2.text == levels[level].syllable2 &&
+            txtSyllable3.text == levels[level].syllable3)
+        {
+            txtSyllable1.layer.borderColor = UIColor .greenColor().CGColor
+            txtSyllable2.layer.borderColor = UIColor .greenColor().CGColor
+            txtSyllable3.layer.borderColor = UIColor .greenColor().CGColor
+            self.addBlurEffect()
+            println("Acertou")
+        }
+        //-------------------------------------------------
+        else
+            
+        // Wrong Answer
+        //-------------------------------------------------
+        {
+            // Confirm Syllable 1
+            if txtSyllable1.text == levels[level].syllable1
+            {
+                txtSyllable1.layer.borderColor = UIColor.greenColor().CGColor
+            }
+            else
+            {
+                txtSyllable1.layer.borderColor = UIColor.redColor().CGColor
+                wrongAnimation(txtSyllable1)
+            }
+            
+            // Confirm Syllable 2
+            if txtSyllable2.text == levels[level].syllable2
+            {
+                txtSyllable2.layer.borderColor = UIColor.greenColor().CGColor
+            }
+            else
+            {
+                txtSyllable2.layer.borderColor = UIColor.redColor().CGColor
+                wrongAnimation(txtSyllable2)
+            }
+            
+            // Confirm Syllable 3
+            if txtSyllable3.text == levels[level].syllable3
+            {
+                txtSyllable3.layer.borderColor = UIColor.greenColor().CGColor
+            }
+            else
+            {
+                txtSyllable3.layer.borderColor = UIColor.redColor().CGColor
+                wrongAnimation(txtSyllable3)
+            }
+        }
+        //-------------------------------------------------
+    }
+    //================================================================================
+    
+    
+    
+    // MARK: - Wrong Answer Animation
+    //================================================================================
+    func wrongAnimation(textFieldAnimate: UITextField)
+    {
+        UIView.animateWithDuration(0.1, delay: 0.1, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+            textFieldAnimate.transform = CGAffineTransformMakeTranslation(self.view.frame.origin.x+20, 0);
+            }) { (finished) -> Void in
+                UIView.animateWithDuration(0.1, delay: 0.1, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                    textFieldAnimate.transform = CGAffineTransformMakeTranslation(self.view.frame.origin.x-20, 0);
+                }, completion: { (finished) -> Void in
+                    UIView.animateWithDuration(0.1, delay: 0.1, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                        textFieldAnimate.transform = CGAffineTransformMakeTranslation(self.view.frame.origin.x+20, 0);
+                    }, completion: { (finished) -> Void in
+                        UIView.animateWithDuration(0.1, delay: 0.1, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                            textFieldAnimate.transform = CGAffineTransformMakeTranslation(0, 0);
+                        }, completion: { (finished) -> Void in
+                            println("Errou")
+                        })
+                    })
+                })
+        }
+    }
+    //================================================================================
+    
+    
+    
+    // MARK: - Blur Effect
+    //================================================================================
+    func addBlurEffect()
+    {
+        var effect = UIBlurEffect(style: UIBlurEffectStyle.Light)
+        var effectView = UIVisualEffectView(effect: effect)
+        effectView.frame = CGRectMake(0, 0, view.bounds.width, view.bounds.height)
+        view.addSubview(effectView)
+    }
+    //================================================================================
+    
 }
