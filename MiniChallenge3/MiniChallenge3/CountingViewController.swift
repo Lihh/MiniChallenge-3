bb750573ee1c337c685347b2d759b3c182ad4924
Lidia Chou <lidia.chou94@gmail.com>
2015-05-26 08:33:30 -0300

    Jogo Counting com os 9 lvls funcionando

--- MiniChallenge3/MiniChallenge3/CountingViewController.swift
+++ MiniChallenge3/MiniChallenge3/CountingViewController.swift
@@ -74,14 +74,84 @@ class CountingViewController: UIViewController {
         /*option  3*/ false, /*  text   */   "4", /*option  4*/  true, /*text     */   "5"),
     
     // Level 2
-        (2, "CLIQUE NOS PASSAROS", 5, "QUANTOS PASSAROS ?", "PASSARO", "CACHORRO",
-        /*button  1*/  true, /*button  2*/ false, /*button  3*/ false, /*button  4*/ false,
-        /*button  5*/ false, /*button  6*/  true, /*button  7*/ false, /*button  8*/ false,
-        /*button  9*/ false, /*button 10*/ false, /*button 11*/  true, /*button 12*/ false,
-        /*button 13*/ false, /*button 14*/ false, /*button 15*/ false, /*button 16*/  true,
-        /*button 17*/ false, /*button 18*/ false, /*button 19*/ false, /*button 20*/  true,
+    (2, "CLIQUE NOS CACHORROS", 4, "QUANTOS CACHORROS ?", "CACHORRO", "PASSARO",
+        /*button  1*/ false, /*button  2*/ false, /*button  3*/  true, /*button  4*/ false,
+        /*button  5*/ false, /*button  6*/ false, /*button  7*/ false, /*button  8*/  true,
+        /*button  9*/  true, /*button 10*/ false, /*button 11*/ false, /*button 12*/ false,
+        /*button 13*/ false, /*button 14*/ false, /*button 15*/ false, /*button 16*/ false,
+        /*button 17*/ false, /*button 18*/ false, /*button 19*/  true, /*button 20*/ false,
         /*option  1*/ false, /*  text   */   "2", /*option  2*/ false, /*  text   */   "3",
-        /*option  3*/ false, /*  text   */   "4", /*option  4*/  true, /*text     */   "5")]
+        /*option  3*/  true, /*  text   */   "4", /*option  4*/ false, /*text     */   "5"),
+    
+    // Level 3
+    (3, "CLIQUE NOS TIGRES", 5, "QUANTOS TIGRES ?", "TIGRE", "PEIXE",
+        /*button  1*/ false, /*button  2*/  true, /*button  3*/ false, /*button  4*/ false,
+        /*button  5*/ false, /*button  6*/ false, /*button  7*/  true, /*button  8*/ false,
+        /*button  9*/ false, /*button 10*/ false, /*button 11*/ false, /*button 12*/  true,
+        /*button 13*/  true, /*button 14*/ false, /*button 15*/ false, /*button 16*/ false,
+        /*button 17*/ false, /*button 18*/ false, /*button 19*/ false, /*button 20*/  true,
+        /*option  1*/ false, /*  text   */   "3", /*option  2*/ false, /*  text   */   "4",
+        /*option  3*/  true, /*  text   */   "5", /*option  4*/ false, /*text     */   "6"),
+    
+    // Level 4
+    (4, "CLIQUE NOS URSOS", 7, "QUANTOS URSOS ?", "URSO", "COELHO",
+        /*button  1*/  true, /*button  2*/ false, /*button  3*/  true, /*button  4*/ false,
+        /*button  5*/ false, /*button  6*/ false, /*button  7*/ false, /*button  8*/  true,
+        /*button  9*/ false, /*button 10*/  true, /*button 11*/ false, /*button 12*/  true,
+        /*button 13*/ false, /*button 14*/ false, /*button 15*/ false, /*button 16*/ false,
+        /*button 17*/  true, /*button 18*/ false, /*button 19*/ false, /*button 20*/  true,
+        /*option  1*/ false, /*  text   */   "6", /*option  2*/  true, /*  text   */   "7",
+        /*option  3*/ false, /*  text   */   "8", /*option  4*/ false, /*text     */   "9"),
+    
+    // Level 5
+    (5, "CLIQUE NOS PEIXES", 8, "QUANTOS PEIXES ?", "PEIXE", "RATO",
+        /*button  1*/ false, /*button  2*/  true, /*button  3*/ false, /*button  4*/  true,
+        /*button  5*/  true, /*button  6*/ false, /*button  7*/  true, /*button  8*/ false,
+        /*button  9*/ false, /*button 10*/  true, /*button 11*/ false, /*button 12*/ false,
+        /*button 13*/ false, /*button 14*/ false, /*button 15*/ false, /*button 16*/ false,
+        /*button 17*/  true, /*button 18*/ false, /*button 19*/  true, /*button 20*/  true,
+        /*option  1*/ false, /*  text   */   "5", /*option  2*/ false, /*  text   */   "6",
+        /*option  3*/ false, /*  text   */   "7", /*option  4*/  true, /*text     */   "8"),
+    
+    // Level 6
+    (6, "CLIQUE NOS MACACOS", 6, "QUANTOS MACACOS ?", "MACACO", "LEAO",
+        /*button  1*/ false, /*button  2*/  true, /*button  3*/  true, /*button  4*/ false,
+        /*button  5*/ false, /*button  6*/ false, /*button  7*/ false, /*button  8*/ false,
+        /*button  9*/ false, /*button 10*/ false, /*button 11*/ false, /*button 12*/ false,
+        /*button 13*/ false, /*button 14*/  true, /*button 15*/  true, /*button 16*/ false,
+        /*button 17*/  true, /*button 18*/ false, /*button 19*/ false, /*button 20*/  true,
+        /*option  1*/ false, /*  text   */   "5", /*option  2*/  true, /*  text   */   "6",
+        /*option  3*/ false, /*  text   */   "7", /*option  4*/ false, /*text     */   "8"),
+    
+    // Level 7
+    (7, "CLIQUE NOS GATOS", 9, "QUANTOS GATOS ?", "GATO", "TIGRE",
+        /*button  1*/ false, /*button  2*/ false, /*button  3*/  true, /*button  4*/ false,
+        /*button  5*/ false, /*button  6*/  true, /*button  7*/ false, /*button  8*/  true,
+        /*button  9*/  true, /*button 10*/ false, /*button 11*/  true, /*button 12*/ false,
+        /*button 13*/ false, /*button 14*/  true, /*button 15*/ false, /*button 16*/  true,
+        /*button 17*/  true, /*button 18*/ false, /*button 19*/  true, /*button 20*/ false,
+        /*option  1*/ false, /*  text   */   "6", /*option  2*/ false, /*  text   */   "7",
+        /*option  3*/ false, /*  text   */   "8", /*option  4*/  true, /*text     */   "9"),
+    
+    // Level 8
+    (8, "CLIQUE NOS CAVALOS", 4, "QUANTOS CAVALOS ?", "CAVALO", "CAMELO",
+        /*button  1*/ false, /*button  2*/  true, /*button  3*/ false, /*button  4*/ false,
+        /*button  5*/ false, /*button  6*/ false, /*button  7*/ false, /*button  8*/ false,
+        /*button  9*/ false, /*button 10*/ false, /*button 11*/  true, /*button 12*/ false,
+        /*button 13*/  true, /*button 14*/ false, /*button 15*/ false, /*button 16*/ false,
+        /*button 17*/ false, /*button 18*/ false, /*button 19*/ false, /*button 20*/  true,
+        /*option  1*/  true, /*  text   */   "4", /*option  2*/ false, /*  text   */   "5",
+        /*option  3*/ false, /*  text   */   "6", /*option  4*/ false, /*text     */   "7"),
+    
+    // Level 9
+    (9, "CLIQUE NOS ELEFANTES", 6, "QUANTOS ELEFANTES ?", "ELEFANTE", "HIPOPOTAMO",
+        /*button  1*/ false, /*button  2*/ false, /*button  3*/ false, /*button  4*/ false,
+        /*button  5*/  true, /*button  6*/  true, /*button  7*/ false, /*button  8*/ false,
+        /*button  9*/ false, /*button 10*/ false, /*button 11*/  true, /*button 12*/ false,
+        /*button 13*/ false, /*button 14*/  true, /*button 15*/ false, /*button 16*/  true,
+        /*button 17*/ false, /*button 18*/ false, /*button 19*/  true, /*button 20*/ false,
+        /*option  1*/ false, /*  text   */   "5", /*option  2*/  true, /*  text   */   "6",
+        /*option  3*/ false, /*  text   */   "7", /*option  4*/ false, /*text     */   "8")]
     //================================================================================
     
     
@@ -459,7 +529,7 @@ class CountingViewController: UIViewController {
                                     }, completion: { (finished) -> Void in
                                         buttonAnimate.backgroundColor = UIColor.greenColor()
                                         buttonAnimate.enabled = false
-                                        if self.finish == 5 { self.showQuestion() }
+                                        if self.finish == self.levels[self.level].finish { self.showQuestion() }
                                 })
                         })
                 })
